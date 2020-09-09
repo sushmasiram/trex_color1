57d8eae1d970e6ea801a54e6b9335470039ebd66
sushmasiram <67284844+sushmasiram@users.noreply.github.com>
2020-09-09 08:27:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,18 +15,18 @@ var gameOver, restart;
 
 
 function preload(){
-  jumpSound = loadSound("assets/sounds/jump.wav")
-  collidedSound = loadSound("assets/sounds/collided.wav")
+  jumpSound = loadSound("jump.wav")
+  collidedSound = loadSound("collided.wav")
   
-  backgroundImg = loadImage("assets/backgroundImg.png")
-  sunAnimation = loadImage("assets/sun.png");
+  backgroundImg = loadImage("backgroundImg.png")
+  sunAnimation = loadImage("sun.png");
   
-  trex_running = loadAnimation("assets/trex_2.png","assets/trex_1.png","assets/trex_3.png");
-  trex_collided = loadAnimation("assets/trex_collided.png");
+  trex_running = loadAnimation("trex_3.png");
+  trex_collided = loadAnimation("trex_collided.png");
   
-  groundImage = loadImage("assets/ground.png");
+  groundImage = loadImage("ground.png");
   
-  cloudImage = loadImage("assets/cloud.png");
+  cloudImage = loadImage("cloud.png");
   
   obstacle1 = loadImage("assets/obstacle1.png");
   obstacle2 = loadImage("assets/obstacle2.png");
